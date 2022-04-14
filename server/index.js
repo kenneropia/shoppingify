57c51e1cfb801f68ad3198d6878434c2540db41f
kenneropia <adetunmbikenny@gmail.com>
2022-04-14 19:23:49 +0100

    new db config

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ process.on('uncaughtException', (err) => {
 
 const DB =
   process.env.NODE_ENV == 'development'
-    ? process.env.MONGODB_LOCAL
+    ? process.env.MONGODB_URL
     : process.env.MONGODB_URL
 
 mongoose
